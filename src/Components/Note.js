21c3f3d76ff36aba506b9e73612854c547c8cd93
Namrata <namratakudache09@gmail.com>
2025-02-02 19:57:12 +0530

    shoppingCartApp

--- src/Components/Note.js
+++ src/Components/Note.js
@@ -1,19 +1,17 @@
-import React from 'react'
+import React from "react";
 
-
-const Note = ( {id, text, date ,handleDeleteNote,color}) => {
+const Note = ({ id, text, date, handleDeleteNote, color }) => {
   return (
-    <div className='note' style={{backgroundColor:color}}>
+    <div className="note" style={{ backgroundColor: color }}>
       <span>{text}</span>
-      <div className='foot'>
+      <div className="foot">
         <small>{date}</small>
-        <button className='delete' onClick={()=> handleDeleteNote(id)}>
-        <i className="fa-sharp fa-solid fa-trash icon"></i>
+        <button className="delete" onClick={() => handleDeleteNote(id)}>
+          <i className="fa-sharp fa-solid fa-trash icon"></i>
         </button>
-
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Note
+export default Note;
